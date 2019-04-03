a5be18503699059cbc0c6caec20c6bf5dd8261c1
alessanderleite <alessander.logistica@gmail.com>
2019-04-03 15:38:27 -0300

    drag the MainActivity to the new package called controller

--- app/src/main/java/br/com/alessanderleite/githubrestapi/controller/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/githubrestapi/controller/MainActivity.java
@@ -1,7 +1,9 @@
-package br.com.alessanderleite.githubrestapi;
+package br.com.alessanderleite.githubrestapi.controller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.alessanderleite.githubrestapi.R;
 
 public class MainActivity extends AppCompatActivity {
 
