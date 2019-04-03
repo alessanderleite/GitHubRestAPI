2c8f78e07b8f50e8c2470503b3426a790401f6f4
alessanderleite <alessander.logistica@gmail.com>
2019-04-03 15:59:57 -0300

    code the Service interface

--- app/src/main/java/br/com/alessanderleite/githubrestapi/api/Service.java
+++ app/src/main/java/br/com/alessanderleite/githubrestapi/api/Service.java
@@ -1,4 +1,10 @@
 package br.com.alessanderleite.githubrestapi.api;
 
-public class Service {
+import br.com.alessanderleite.githubrestapi.model.ItemResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface Service {
+    @GET("/search/users?q=language:java+location:brazil")
+    Call<ItemResponse> getItens();
 }
