1318a39989cce869982f369c1b6ae794b1007e40
alessanderleite <alessander.logistica@gmail.com>
2019-04-03 23:50:37 -0300

    start code the MainActivity

--- app/src/main/java/br/com/alessanderleite/githubrestapi/controller/MainActivity.java
+++ app/src/main/java/br/com/alessanderleite/githubrestapi/controller/MainActivity.java
@@ -1,15 +1,82 @@
 package br.com.alessanderleite.githubrestapi.controller;
 
+import android.app.ProgressDialog;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Display;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import br.com.alessanderleite.githubrestapi.R;
+import br.com.alessanderleite.githubrestapi.api.Client;
+import br.com.alessanderleite.githubrestapi.api.Service;
+import br.com.alessanderleite.githubrestapi.model.Item;
+import br.com.alessanderleite.githubrestapi.model.ItemResponse;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    TextView Disconnected;
+    private Item item;
+    ProgressDialog pd;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initViews();
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        swipeContainer.setColorSchemeResources(android.R.color.holo_orange_dark);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadJSON();
+                Toast.makeText(MainActivity.this, "Github Users Refreshed", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void initViews() {
+        pd = new ProgressDialog(this);
+        pd.setMessage("Fetching Github Users...");
+        pd.setCancelable(false);
+        pd.show();
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+        loadJSON();
+    }
+
+    private void loadJSON() {
+        Disconnected = (TextView) findViewById(R.id.disconnected);
+        try {
+            Client client = new Client();
+            Service apiService =
+                    client.getClient().create(Service.class);
+            Call<ItemResponse> call = apiService.getItems();
+            call.enqueue(new Callback<ItemResponse>() {
+                @Override
+                public void onResponse(Call<ItemResponse> call, Response<ItemResponse> response) {
+
+                }
+
+                @Override
+                public void onFailure(Call<ItemResponse> call, Throwable t) {
+
+                }
+            });
+
+        } catch (Exception e) {
+
+        }
     }
 }
